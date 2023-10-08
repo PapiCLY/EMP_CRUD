32804df41ba5073cc6439e75820ad22f621690d0
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-10-08 17:49:40 -0400

    minor glitch with sql duplicate error

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ employeeRoutes = require('./controllers/employeeControllers')
 //middleware
 app.use('/api/employees', employeeRoutes)
 
+//global error catcher from express docs
+app.use((err,req,res,next) => {
+    console.log(err)
+    res.status(err.status || 500).send('something went wrong')
+})
+
 db.query("SELECT 1")
 .then(data => {
     console.log('db connection succeeded')
     app.listen(3000, 
         ()=>console.log('server started at 3000'))
 })
-.catch(err=>console.log('db connection failed. \n' + err))
+.catch(err=>console.log('db connection failed' + err))
