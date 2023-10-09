e6e3f90d2f1ef3f853ddc2be12e8791c01efc163
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-10-09 00:10:00 -0400

    added sql commands for reference

--- services/employeeService.js
+++ services/employeeService.js
@@ -27,3 +27,30 @@ module.exports.addOrEditEmployee = async (obj,id = 0)=>{
     [id,obj.name,obj.position,obj.location,obj.salary])//this syntax is recommended per the mysql spm documentation. This is more secure
     return affectedRows;
 }
+
+// SQL commands for handling update
+
+// CREATE DEFINER=`root`@`localhost` PROCEDURE `usp_employee_add_or_edit`(
+// IN _id INT,
+// IN _name VARCHAR(255),
+// IN _position VARCHAR(255),
+// IN _location VARCHAR(255),
+// IN _salary INT
+// )
+// BEGIN
+// 	IF _id = 0 THEN
+// 		INSERT INTO employees(name,position,location,salary)
+//         VALUES (_name,_position,_location,_salary);
+        
+//     ELSE 
+// 		UPDATE employees
+//         SET name = _name,
+//         position = _position,
+//         location = _location,
+//         salary = _salary
+//         WHERE id = _id;
+//     END IF;
+    
+    
+//     SELECT ROW_COUNT() AS `affectedRows`;
+// END
